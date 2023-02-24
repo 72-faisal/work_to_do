023fc0d7e698e398ea335b5ba57a07c73bba9c51
72-faisal <vhorafaisal777@gmail.com>
2023-02-24 12:05:39 +0530

    task 11

---
+++ do_tasks/src/do_tasks/Display_system_time.java
@@ -0,0 +1,14 @@
+package do_tasks;
+
+import java.util.Date;
+
+public class Display_system_time {
+	
+	public static void main(String[] args) {
+		Date display = new Date();
+//		long timeMillis = display.getTime();
+		System.out.println("System time : " +display);
+	    
+	}
+
+}
