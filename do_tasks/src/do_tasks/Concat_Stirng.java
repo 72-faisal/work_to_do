286998a38b4fafde3bc81b3e67c7ce5ad388764a
72-faisal <vhorafaisal777@gmail.com>
2023-02-24 19:47:25 +0530

    from task 11 to 19

---
+++ do_tasks/src/do_tasks/Concat_Stirng.java
@@ -0,0 +1,12 @@
+package do_tasks;
+
+public class Concat_Stirng {
+	
+	public static void main(String[] args) {
+	        String str1 = "Hello";
+	        String str2 = "world!";
+	        
+	        System.out.println("Result string: " + str1.concat(str2));
+	    }
+	}
+
